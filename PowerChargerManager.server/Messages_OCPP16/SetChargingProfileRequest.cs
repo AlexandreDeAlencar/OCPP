bdc13d7c9a885c99399c8b56ffdd2e57fc16d26a
maken5 <marcio@mkdinfo.com.br>
2023-06-17 20:50:46 -0300

    adicioando projeto de migração inicial do ocpp server para net6

---
+++ PowerChargerManager.server/Messages_OCPP16/SetChargingProfileRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace OCPP.Core.Server.Messages_OCPP16
+{
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.3.1.0 (Newtonsoft.Json v9.0.0.0)")]
+    public class SetChargingProfileRequest
+    {
+        [Newtonsoft.Json.JsonProperty("connectorId", Required = Newtonsoft.Json.Required.Always)]
+        public int ConnectorId { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("csChargingProfiles", Required = Newtonsoft.Json.Required.Always)]
+        [System.ComponentModel.DataAnnotations.Required]
+        public OCPP.Core.Server.ValueObjects.ChargingProfile CsChargingProfiles { get; set; }
+    }
+}
---
+++ PowerChargerManager.server/ValueObjects/ChargingSchedule.cs
@@ -0,0 +1,34 @@
+﻿namespace OCPP.Core.Server.ValueObjects
+{
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.3.1.0 (Newtonsoft.Json v9.0.0.0)")]
+    public class ChargingSchedule
+    {
+        [Newtonsoft.Json.JsonProperty("duration", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public int Duration { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("startSchedule", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.DateTimeOffset? StartSchedule { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("chargingRateUnit", Required = Newtonsoft.Json.Required.Always)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+        public ChargingRateUnitType ChargingRateUnit { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("chargingSchedulePeriod", Required = Newtonsoft.Json.Required.Always)]
+        [System.ComponentModel.DataAnnotations.Required]
+        public System.Collections.Generic.ICollection<ChargingSchedulePeriod> ChargingSchedulePeriod { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("minChargingRate", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public decimal MinChargingRate { get; set; }
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.3.1.0 (Newtonsoft.Json v9.0.0.0)")]
+    public enum ChargingRateUnitType
+    {
+        [System.Runtime.Serialization.EnumMember(Value = @"W")]
+        W = 0,
+
+        [System.Runtime.Serialization.EnumMember(Value = @"A")]
+        A = 1,
+    }
+}
